6129fe825fa7ffb867b4726651378fd75c914834
seonmin <seonmin1414@gmail.com>
2024-07-01 13:02:31 +0900

    2024. 07. 01 (월) stx 홈페이지
    html(고객문의창, about us, 푸터, 탑버튼 추가) , css 추가, js (탑버튼 회전, 및 상하 이동버튼)추가

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.onload =function(){
-    // 비주얼 이동 버튼 스크롤 기능
+    // 비주얼 이동 버튼 스크롤 기능=======================
     // 변수 선언
     const visualButton = document.querySelector(".visual-bt");
     visualButton.addEventListener("click" , function(){
@@ -15,4 +15,33 @@ function scrollToSection(sectionId) {
         section.scrollIntoView({ behavior:"smooth" });
     }
   }
+//   top버튼 스크롤 기능==================================
+const topBtn = document.getElementById("top-btn");
+topBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log(window.scrollY);
+if (window.scrollY == 0) {
+    window.scrollTo({
+        top:2777,
+        behavior:"smooth"
+    });
+    }
+    else{
+        window.scrollTo({
+            top:0,
+            behavior:"smooth"
+        })
+    }
+});
+// 화샇표 이미지 회전(창이 끝까지 내려갔을때 화살표의 방향이 반대쪽으로 바뀌기)
+const topBtnImg = document.getElementById("top-btn-img");
+window.addEventListener("scroll", function(scTop) {
+    scTop = window.document.documentElement.scrollTop;
+    if(scTop > 0){
+        topBtnImg.classList.add("up")  
+    }
+    else{
+        topBtnImg.classList.remove("up")
+    }
+});
 };
